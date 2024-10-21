fa909893d4a355982337360834034c007d675c65
Willian Luis Grzybowski De Lima <williangrzybowski@gmail.com>
2024-10-21 16:41:15 -0300

    adjusting error in the images and icons, on pages and navbar

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path"); //trabalhar com os caminhos das pag abertas
 const app = express(); //criando objeto app
- 
+
+app.use(express.static(path.join(__dirname, 'public')));
 // Configuração do EJS como view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs"); 
@@ -9,7 +10,6 @@ app.set("view engine", "ejs");
 // Rota principal
 const indexRouter = require("./routes/index");
 app.use("/", indexRouter);
-app.use(express.static(path.join(__dirname,'public')));
  
 // Rotas para alunos
 const alunosRouter = require("./routes/alunos");
